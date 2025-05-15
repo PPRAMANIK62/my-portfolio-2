01639acdc88061e42337e0cf6827d12d21395bee
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-05-15 19:04:02 +0530

    upgrade to tailwind v4

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-[48px] w-full rounded-md border border-white/10 bg-primary px-4 py-5 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white/60 focus:border-accent font-light outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-[48px] w-full rounded-md border border-white/10 bg-primary px-4 py-5 text-base shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white/60 focus:border-accent font-light outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -65,7 +65,7 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-6 top-10 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      <SheetPrimitive.Close className="absolute right-6 top-10 rounded-xs opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-hidden focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="text-3xl text-accent" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
